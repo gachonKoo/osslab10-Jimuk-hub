40c12fa8f38de326ebdaa70b21e2b258b75e415c
Jimuk-hub <dlawlanr0187@naver.com>
2024-11-09 17:52:20 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,12 +1,18 @@
+import sys
+
 def find_divisors(number):
-   
+
     divisors = []
     
-   
     for i in range(1, number + 1):
-      
         if number % i == 0:
-            divisors.append(i)
-    
-   
+            divisors.append(str(i))
     return divisors
+
+if __name__ == "__main__":
+
+    number = int(sys.argv[1])
+
+    divisors = find_divisors(number)
+
+    print(" ".join(divisors))
