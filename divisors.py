033ae384984bfbc6d0a0fb764dab0b2d84836b56
Jimuk-hub <dlawlanr0187@naver.com>
2024-11-09 16:12:35 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -10,12 +10,3 @@ def find_divisors(number):
     
    
     return divisors
-
-
-try:
-    number = int(input("숫자를 입력하세요: "))
-    
-  
-    print(f"{number}의 약수는: {find_divisors(number)}")
-except ValueError:
-    print("올바른 숫자를 입력해주세요.")
